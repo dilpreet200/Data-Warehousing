37862cff861c487385ab1fcd7a72ef2839e65b33
dilpreet200 <93183435+dilpreet200@users.noreply.github.com>
2021-10-26 13:04:00 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,37 +1,29 @@
 import configparser
-import psycopg2 # postgresql adapter/connector for python programming language
-
-"""" import mysql.connector
-
-conn = mysql.connector.connect(user='scott', password='password',
-                              host='127.0.0.1',
-                              database='employees')
-conn.close() """
-
+import psycopg2 
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
-        cur.execute(query) # object "cur" used to call execute method from cursor class of psycopg2 module
+        cur.execute(query) 
         conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
-        conn.commit() # This method sends a COMMIT statement to the postgresql server, committing the current transaction
+        conn.commit() 
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())) # Creating a new database session and returns a new connection object into "conn"
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())) 
     
-    # value() to fetch values from dictionary
+   
     
-    cur = conn.cursor() # cursor position
+    cur = conn.cursor() 
     
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
